5d482add4e272081c3258d820f970b44494e7baa
Desktop42 <36649761+Metafern@users.noreply.github.com>
2021-01-23 20:40:43 -0800

    Update Responder.h

--- XTransrecieverPlus/include/Responder.h
+++ XTransrecieverPlus/include/Responder.h
@@ -12,7 +12,7 @@ public:
 	void setParser(Parser& parserIn);
 	bool getResp(std::vector<pcpp::Packet>& out);
 	int stage = HANDSHAKE;
-	
+	Responder() {};
 private:
 	Parser *parser;
 	int protocol = -1;
@@ -20,5 +20,6 @@ private:
 	bool hasResp = false;
 
 	crft::Lan lan;
+	crft::Station station;
 	void parseLan(std::vector<pcpp::Packet>& packet);
 };
